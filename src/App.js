6754222d18ea4b8da168fd5b1af056efc2be8643
Danyelss <mos.dani@yahoo.com>
2021-11-24 14:10:17 +0200

    Still problems but commited

--- src/App.js
+++ src/App.js
@@ -3,19 +3,9 @@ import React, { useState } from 'react';
 import Routes from './routes';
 import AppLayout from './layouts/AppLayout';
 import ThemeProvider from './contexts/ThemeContext';
-import LoginPage from './pages/LoginPage';
 import './styles/index.scss';
-import getToken from './token/Tokens';
-import setToken from './token/Tokens';
 
 function App() {
-
-    const token = getToken();
-
-    if(!token) {
-        return <LoginPage setToken={setToken} />
-      }
-
   return (
     <ThemeProvider>
       <AppLayout>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,20 @@
 import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import getToken from './token/Tokens';
+import setToken from './token/Tokens';
+
 const HomePage = lazy(() => import('../pages/HomePage'));
 const ErrorPage = lazy(() => import('../pages/ErrorPage'));
 const LoginPage = lazy(() => import('../pages/LoginPage'));
 const Routes = () => {
+
+    const token = getToken();
+
+    if(!token) {
+        return <LoginPage setToken={setToken} />
+      }
+
   return (
     <Suspense fallback={<div />}>
       <Switch>
