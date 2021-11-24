c18d7e91e6c393882546d8c5b44f470d766b2311
Danyelss <mos.dani@yahoo.com>
2021-11-24 15:08:36 +0200

    Changes on routes/index.js, not working

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,49 @@
 import React, { Suspense, lazy } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import {    Switch,
+            Route,
+            BrowserRouter
+        } from 'react-router-dom';
 
-import getToken from './token/Tokens';
-import setToken from './token/Tokens';
+import getToken from '../token/Tokens';
+import setToken from '../token/Tokens';
 
 const HomePage = lazy(() => import('../pages/HomePage'));
 const ErrorPage = lazy(() => import('../pages/ErrorPage'));
 const LoginPage = lazy(() => import('../pages/LoginPage'));
+
+function PrivateRoute({ children }) {
+  const auth = getToken(); /////////////////////////////////////////
+  return auth ? children : <Navigate to="/login" />;
+}
+
 const Routes = () => {
 
-    const token = getToken();
-
-    if(!token) {
-        return <LoginPage setToken={setToken} />
-      }
-
   return (
-    <Suspense fallback={<div />}>
+  <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/private"
+            element={
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+
+  );
+};
+
+/*
+<Suspense fallback={<div />}>
       <Switch>
         <Route exact path="/home" component={HomePage} />
         <Route exact path="/login" component={LoginPage} />
         <Route component={(_) => <ErrorPage status={404} message="Page not found." />} />
       </Switch>
     </Suspense>
-  );
-};
+    */
 
 export default Routes;
